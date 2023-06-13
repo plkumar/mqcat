be0ff2e680a66754266fced92bbd94da262d9560
Lakshman Peethani <lakshman_peethani@epam.com>
2023-06-13 18:25:46 +0530

    refactored the code to cleanup a bit.

--- Commands/PublishCommand.cs
+++ Commands/PublishCommand.cs
@@ -8,10 +8,10 @@ namespace mqcat.Commands;
 public class PublishCommand : Command
 {
     private const int ErrorExitCode = -1;
-    private readonly Option<string> hostOption = new(aliases: new string[]  {"--host", "-h"}, description: "Host name to connect.");
-    private readonly Option<string> messageOption = new(aliases: new string[]  {"--message", "-m"}, description: "Message to publish");
-    private readonly Option<string> exchangeOption = new(aliases: new string[]  {"--exchange", "-e"}, description: "Message to publish");
-    private readonly Option<string> routingKeyOption = new(aliases: new string[]  {"--routing-key", "-r"}, description: "Message to publish");
+    private readonly Option<string> hostOption = new(aliases: new[]  {"--host", "-h"}, description: "Host name to connect.");
+    private readonly Option<string> messageOption = new(aliases: new[]  {"--message", "-m"}, description: "Message to publish");
+    private readonly Option<string> exchangeOption = new(aliases: new[]  {"--exchange", "-e"}, description: "Message to publish");
+    private readonly Option<string> routingKeyOption = new(aliases: new[]  {"--routing-key", "-r"}, description: "Message to publish");
 
     public PublishCommand() : base("publish", "Publishes messages to queue")
     {
@@ -27,47 +27,41 @@ public class PublishCommand : Command
         messageOption.IsRequired=false;
         this.AddOption(messageOption);
        
-        this.SetHandler<string, string, string, string>((host, exchange, routingKey,message)=>{
-            Publish(host,exchange, routingKey, message);
-        }, hostOption, exchangeOption, routingKeyOption, messageOption);
+        this.SetHandler(Publish, hostOption, exchangeOption, routingKeyOption, messageOption);
     }
 
     void Publish(string host, string exchangeName, string routingKey, string message)
     {
-        System.Console.WriteLine($" {host}, {exchangeName}, {routingKey}");
+        Console.WriteLine($" {host}, {exchangeName}, {routingKey}");
 
         var factory = new ConnectionFactory() { Uri=new Uri(host) };
-        using (var connection = factory.CreateConnection())
+        using var connection = factory.CreateConnection();
+        if (string.IsNullOrEmpty(message) && Console.IsInputRedirected)
         {
-            if (string.IsNullOrEmpty(message) && Console.IsInputRedirected)
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(8192))); // This will allow input >256 chars
+            while (Console.In.Peek() != -1)
             {
-                Console.SetIn(new StreamReader(Console.OpenStandardInput(8192))); // This will allow input >256 chars
-                while (Console.In.Peek() != -1)
-                {
-                    message+= Console.In.ReadLine();
-                }
-            } else {
-                System.Console.Error.WriteLine("No Message passes or input redirection detected.");
-                Environment.Exit(ErrorExitCode);
+                message+= Console.In.ReadLine();
             }
-
-            Console.WriteLine("Message : \n " + message + "\n");
-
-            using (var channel = connection.CreateModel())
-            {
-                // Declare the exchange                
-                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic, true);
-
-                // Publish a message
-                byte[] body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(exchange: exchangeName,
-                                     routingKey: routingKey,
-                                     basicProperties: null,
-                                     body: body);
-
-                //Console.WriteLine("Message sent: {0}", message);
-            }
+        } else {
+            Console.Error.WriteLine("No Message passes or input redirection detected.");
+            Environment.Exit(ErrorExitCode);
         }
+
+        Console.WriteLine("Message : \n " + message + "\n");
+
+        using var channel = connection.CreateModel();
+        // Declare the exchange                
+        channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic, true);
+
+        // Publish a message
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: exchangeName,
+            routingKey: routingKey,
+            basicProperties: null,
+            body: body);
+
+        //Console.WriteLine("Message sent: {0}", message);
     }
 }
