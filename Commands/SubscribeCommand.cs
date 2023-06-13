7f91407ff653dd1c28e5792c6d6d3e5ac1738353
Lakshman Peethani <lakshman_peethani@epam.com>
2023-06-13 18:25:46 +0530

    refactored the code to cleanup a bit.

--- Commands/SubscribeCommand.cs
+++ Commands/SubscribeCommand.cs
@@ -7,10 +7,10 @@ namespace mqcat.Commands;
 
 public class SubscribeCommand : Command
 {
-    private readonly Option<string> hostOption = new(aliases: new string[]  {"--host", "-h"}, description: "Host name to connect.");
-    private readonly Option<string> queueOption = new(aliases: new string[]  {"--queue", "-q"}, description: "Queue name to publish message into.");
-    private readonly Option<bool> isDurableOption = new(aliases: new string[] {"--durable", "-d"}, "Is queue durable.");
-    private readonly Option<bool> waitOption = new(aliases: new string[] {"--wait", "-w"}, "shoud command wait for new messages.");
+    private readonly Option<string> hostOption = new(aliases: new[]  {"--host", "-h"}, description: "Host name to connect.");
+    private readonly Option<string> queueOption = new(aliases: new[]  {"--queue", "-q"}, description: "Queue name to publish message into.");
+    private readonly Option<bool> isDurableOption = new(aliases: new[] {"--durable", "-d"}, "Is queue durable.");
+    // private readonly Option<bool> waitOption = new(aliases: new[] {"--wait", "-w"}, "should command wait for new messages.");
 
     public SubscribeCommand() : base("subscribe", "Subscribes to message queue.")
     {
@@ -19,12 +19,10 @@ public class SubscribeCommand : Command
         queueOption.IsRequired=true;
         this.AddOption(queueOption);
 
-        isDurableOption.SetDefaultValue(false);
+        isDurableOption.SetDefaultValue(value: false);
         this.AddOption(isDurableOption);
 
-        this.SetHandler<string, string, bool>((host, queueName, isDurable)=>{
-            Subscribe(host, queueName, isDurable);
-        }, hostOption, queueOption, isDurableOption);
+        this.SetHandler(handle: Subscribe, hostOption, queueOption, isDurableOption);
     }
 
     private void Subscribe(string host, string queueName, bool isDurable)
@@ -38,14 +36,14 @@ public class SubscribeCommand : Command
 
                 // Set up a consumer to receive messages
                 var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (sender, ea) =>
+                consumer.Received += (_, ea) =>
                 {
                     byte[] body = ea.Body.ToArray();
                     string message = Encoding.UTF8.GetString(body);
                     Console.WriteLine("Received message: {0}", message);
                 };
 
-                channel.QueueBind(queueName, "demo-exchange", "/temp/#");
+                // channel.QueueBind(queueName, "demo-exchange", "/temp/#");
                 // Start consuming messages from the queue
                 channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
             }
