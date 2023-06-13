afccce3c9e1852bd4ed70337412d06fb834f5570
Lakshman Kumar Peethani <peethani@outlook.com>
2023-06-13 10:22:19 +0530

    updated SubscribeCommand for durable option

--- Commands/SubscribeCommand.cs
+++ Commands/SubscribeCommand.cs
@@ -11,6 +11,8 @@ public class SubscribeCommand : Command
 {
     private readonly Option<string> hostOption = new(aliases: new string[]  {"--host", "-h"}, description: "Host name to connect.");
     private readonly Option<string> queueOption = new(aliases: new string[]  {"--queue", "-q"}, description: "Queue name to publish message into.");
+    private readonly Option<bool> isDurableOption = new(aliases: new string[] {"--durable", "-d"}, "Is queue durable.");
+    private readonly Option<bool> waitOption = new(aliases: new string[] {"--wait", "-w"}, "shoud command wait for new messages.");
 
     public SubscribeCommand() : base("subscribe", "Subscribes to message queue.")
     {
@@ -18,19 +20,23 @@ public class SubscribeCommand : Command
         this.AddOption(hostOption);
         queueOption.IsRequired=true;
         this.AddOption(queueOption);
-        this.SetHandler<string, string>((host, queueName)=>{
-            Subscribe(host, queueName);
-        }, hostOption, queueOption);
+
+        isDurableOption.SetDefaultValue(false);
+        this.AddOption(isDurableOption);
+
+        this.SetHandler<string, string, bool>((host, queueName, isDurable)=>{
+            Subscribe(host, queueName, isDurable);
+        }, hostOption, queueOption, isDurableOption);
     }
 
-    private void Subscribe(string host, string queueName)
+    private void Subscribe(string host, string queueName, bool isDurable)
     {
         var factory = new ConnectionFactory() { HostName = host };
         using (var connection = factory.CreateConnection())
         {
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: queueName, durable: isDurable, exclusive: false, autoDelete: false, arguments: null);
 
                 // Set up a consumer to receive messages
                 var consumer = new EventingBasicConsumer(channel);
