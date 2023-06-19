d145b95a15a9c30e3e321b3b9a5b291ed2446393
Lakshman Peethani <lakshman_peethani@epam.com>
2023-06-19 23:14:02 +0530

    refactored code and added sonar analysis.

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# mqcat
+# mqcat - a simple tool to publish and subscribe messages from AMQP Broker queues
+
+## Build
+
+```bash
+dotnet build
+```
+
+## Run
+
+```bash
+dotnet run --project src/mqcat/mqcat.csproj -- --help
+```
+
+## Usage
+
+```bash
+mqcat [command] [options]
+```
+
+### Commands
+
+#### publish
+
+Publish a message to a queue.
+
+```bash
+mqcat publish [options]
+```
+
+##### Options
+
+| Option | Description | Default |
+| --- | --- | --- |
+| -h, --host | AMQP Broker host | localhost |
+| -p, --port | AMQP Broker port | 5672 |
+| -u, --user | AMQP Broker user | guest |
+| -P, --password | AMQP Broker password | guest |
+| -q, --queue | Queue name | |
+| -m, --message | Message to publish | |
+| -f, --file | File to publish | |
+| -e, --exchange | Exchange name | |
+| -r, --routing-key | Routing key | |
+|--- | --- | --- |
+
+#### subscribe
+
+Subscribe to a queue and print messages to stdout.
+
+```bash
+mqcat subscribe [options]
+```
+
+##### Options
+
+| Option | Description | Default |
+| --- | --- | --- |
+| -h, --host | AMQP Broker host | localhost |
+| -p, --port | AMQP Broker port | 5672 |
+| -u, --user | AMQP Broker user | guest |
+| -P, --password | AMQP Broker password | guest |
+| -q, --queue | Queue name | |
+| -e, --exchange | Exchange name | |
+| --- | --- | --- |
+
+## License
+
+MIT
+``` 
+```
+
--- Util/Utils.cs
+++ Util/Utils.cs
@@ -1,3 +1,5 @@
+using System.Security;
+
 namespace mqcat.Util;
 
 public class Utils
@@ -5,7 +7,7 @@ public class Utils
     public static void LogError(string text)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.Error.WriteLine(text);
+        Console.Error.WriteLine($"ERROR :: {text}");
         Console.ResetColor();
     }
     
@@ -21,5 +23,55 @@ public class Utils
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(text);
         Console.ResetColor();
+    }
+
+    // public SecureString GetPassword()
+    // {
+    //     var pwd = new SecureString();
+    //     while (true)
+    //     {
+    //         ConsoleKeyInfo i = Console.ReadKey(true);
+    //         if (i.Key == ConsoleKey.Enter)
+    //         {
+    //             break;
+    //         }
+    //         else if (i.Key == ConsoleKey.Backspace)
+    //         {
+    //             if (pwd.Length > 0)
+    //             {
+    //                 pwd.RemoveAt(pwd.Length - 1);
+    //                 Console.Write("\b \b");
+    //             }
+    //         }
+    //         else if (i.KeyChar != '\u0000' ) // KeyChar == '\u0000' if the key pressed does not correspond to a printable character, e.g. F1, Pause-Break, etc
+    //         {
+    //             pwd.AppendChar(i.KeyChar);
+    //             Console.Write("*");
+    //         }
+    //     }
+    //     return pwd;
+    // }
+    public static string ReadPassword()
+    {
+        var pass = string.Empty;
+        ConsoleKey key;
+        do
+        {
+            var keyInfo = Console.ReadKey(intercept: true);
+            key = keyInfo.Key;
+
+            if (key == ConsoleKey.Backspace && pass.Length > 0)
+            {
+                Console.Write("\b \b");
+                pass = pass[0..^1];
+            }
+            else if (!char.IsControl(keyInfo.KeyChar) )
+            {
+                Console.Write("*");
+                pass += keyInfo.KeyChar;
+            }
+        } while (key != ConsoleKey.Enter);
+
+        return pass;
     }
 }
